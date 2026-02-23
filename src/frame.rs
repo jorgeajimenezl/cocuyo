6f2518088cd12ea32f7cea718fc58ae732b854cf
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2026-02-23 09:54:16 +0100

    feat(windows): simplify shared texture slot reference in FrameData

--- src/frame.rs
+++ src/frame.rs
@@ -5,6 +5,9 @@ use std::sync::Arc;
 #[cfg(target_os = "linux")]
 use drm_fourcc::DrmFourcc;
 
+#[cfg(target_os = "windows")]
+use crate::platform::windows::shared_texture;
+
 impl std::fmt::Debug for FrameData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -52,7 +55,7 @@ pub enum FrameData {
     },
     #[cfg(target_os = "windows")]
     D3DShared {
-        slot: Arc<crate::platform::windows::shared_texture::SharedTextureSlot>,
+        slot: Arc<shared_texture::SharedTextureSlot>,
         width: u32,
         height: u32,
     },
